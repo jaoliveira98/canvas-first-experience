3b61d08f3f6e132902d8b705a6d39b44ae342840
Joao Oliveira <86269259+jaoliveira98@users.noreply.github.com>
2025-07-15 11:42:40 +0100

    cleanup + added 3 circles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { drawCircle } from "./circle";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  useEffect(() => {
+    const canvas = canvasRef.current;
 
-export default App
+    if (!canvas) return;
+
+    // Get dimensions of the canvas
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+
+    const ctx = canvas.getContext("2d");
+
+    if (ctx) {
+      drawCircle(ctx, 100, 100, 50, "red");
+      drawCircle(ctx, 350, 100, 50, "yellow");
+      drawCircle(ctx, 600, 100, 50, "green");
+    }
+  }, []);
+
+  return <Canvas ref={canvasRef} id="canvas"></Canvas>;
+};
+
+const Canvas = styled.canvas`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(246, 248, 253);
+`;
+
+export default App;
